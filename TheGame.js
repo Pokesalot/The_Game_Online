2544ff2cc225df52ff8a048c6cacd811948d3c94
Pokesalot <athankinson@gmail.com>
2021-01-11 02:59:57 -0500

    (update) Advanced Fish AI added

--- TheGame.js
+++ TheGame.js
@@ -23,6 +23,8 @@ let deck = [2];//Skip adding 2 'randomly' since it will be the only card
 for(let i = 3; i <= 99; i++){
     deck.splice(Math.floor(Math.random() * (deck.length + 1)),0,i);
 }
+let masterDeck = [];for(let i=0;i<deck.length;i++){masterDeck.push(deck[i])}; let simulationsRun=[];
+let deckID = Math.random().toString().split(".")[1];
 //Create piles
 let piles = {};
 piles[0] = 1;
@@ -41,9 +43,11 @@ let lastPlay = {"Put":0,"Pile":0,"Last":0};
 let undos = 0;
 let cardsPlayed = 0;
 
-PopulatePiles()
-PopulateHand()
-PopulateDifficulties()
+PopulatePiles();
+PopulateHand();
+PopulateDifficulties();
+PopulateBingo();
+SimulateCurrentGame(0);SimulateCurrentGame(1);SimulateCurrentGame();
 
 
 function PopulateHand(){
@@ -60,11 +64,11 @@ function PopulateHand(){
     for(let i =0;i<hand.length;i++){
         if(hand[i] != selected){
             handHTML += `
-                <td onclick="SelectFromHand(${hand[i]})">${hand[i]}</td>
+                <td class="middle" onclick="SelectFromHand(${hand[i]})">${hand[i]}</td>
             `
         }else{
             handHTML += `
-                <td style="background-color: ${selectionColor};" onclick="SelectFromHand(${hand[i]})">${hand[i]}</td>
+                <td class="middle" style="background-color: ${selectionColor};" onclick="SelectFromHand(${hand[i]})">${hand[i]}</td>
             `
         }
     }
@@ -80,6 +84,27 @@ function PopulatePiles(){
         td.innerText= piles[pile];
     }
 }
+
+function PopulateDifficulties(){
+    let options = "";
+    for(let i=0;i<difficulties.length;i++){
+        options += `<option>Level ${i + 1}</option>`;
+    }
+    $("NewGameSelector").innerHTML = options;
+}
+
+function PopulateBingo(){
+    let boardHTML = "<tr class=\"right\">";
+    for(let i=0;i<10;i++){
+        for(let j=1;j<=10;j++){
+            let num = i*10 + j;
+            boardHTML += `<td class=\"right\"${hand.indexOf(num) + deck.indexOf(num) == -2?" style=\"background-color: black\"":""}>${num}</td>`;
+        }
+        boardHTML += "</tr><tr class=\"right\">";
+    }
+    $("BingoBoard").innerHTML = boardHTML + "</tr>";
+}
+
 
 function $(id){
     return document.getElementById(id)
@@ -148,6 +173,7 @@ function TryPlay(pileNumber){
     selected = 0
     PopulatePiles();
     PopulateHand();
+    PopulateBingo();
     CheckForDeadBoard();
 }
 
@@ -207,7 +233,8 @@ function NewGame(){
         if(hand.indexOf(i) > -1){continue;}
         deck.splice(Math.floor(Math.random() * deck.length + 1),0,i);
     }
-
+    masterDeck = [];for(let i=0;i<deck.length;i++){masterDeck.push(deck[i])}; simulationsRun=[];
+    deckID = Math.random().toString().split(".")[1];
     selected = 0;
     PlayedThisTurn = 0;
     lastPlay = {"Put":0,"Pile":0,"Last":0};
@@ -217,10 +244,12 @@ function NewGame(){
     $("playsLeftHeader").innerText = `Cards to play: ${MinPlaysWithDeck}`
     $("EndTurnButton").hidden = true;
     $("NoMoves").hidden=true;
-    $("ScoreBar").innerText = "Score: 0"
+    $("ScoreBar").innerText = "Score: 0";
 
-    PopulatePiles()
-    PopulateHand()
+    PopulatePiles();
+    PopulateHand();
+    PopulateBingo();
+    SimulateCurrentGame(0);SimulateCurrentGame(1);SimulateCurrentGame();
 }
 
 function EndTurn(){
@@ -238,18 +267,12 @@ function EndTurn(){
     CheckForDeadBoard();
 }
 
-function PopulateDifficulties(){
-    let options = "";
-    for(let i=0;i<difficulties.length;i++){
-        options += `<option>Level ${i + 1}</option>`;
-    }
-    $("NewGameSelector").innerHTML = options;
-}
-
 function CheckForDeadBoard(){
     if(hand.length >= 1){
-        let endturn = $("EndTurnButton");
-        if(hand[0] > piles[2] && hand[0] > piles[3] && hand[hand.length-1]<piles[0] && hand[hand.length-1]<piles[1] && endturn.hidden){//No possible moves and we can't end turn
+        for(let i=0;i<hand.length;i++){//We don't check for valid moves in the form of moving the wrong way by ten below, but those are valid moves
+            if(hand[i] == piles[0]-10 || hand[i] == piles[1]-10 || hand[i] == piles[2]+10 ||hand[i] == piles[3]+10){return;}
+        }
+        if(hand[0] > piles[2] && hand[0] > piles[3] && hand[hand.length-1]<piles[0] && hand[hand.length-1]<piles[1] && $("EndTurnButton").hidden){//No possible moves and we can't end turn
             let charlie = $("NoMoves");
             charlie.hidden=false;
             setTimeout(() => {  alert("Your game has no more moves, let's start a new one!");NewGame() }, 500);
@@ -271,4 +294,89 @@ function UndoMove(){
     PopulatePiles();
     $("UndoButton").hidden = true;
     $("ScoreBar").innerText = `Score: ${score}`
+}
+
+function SimulateCurrentGame(ratio=Math.random()){
+    let curDeckID = deckID; let DeadGame=false;
+    let simDeck=[];for(let i=0;i<masterDeck.length;i++){simDeck.push(masterDeck[i])};
+    let simPiles = [];
+    let simHand = [];
+    let simScore = 0;let simCardsPlayed=0;
+    simPiles[0] = 1;simPiles[1]=1;simPiles[2]=100;simPiles[3]=100;
+
+    //TODO on stream, figure out how to play the game
+    while((simDeck.length > 0 || simHand.length > 0) && !DeadGame){
+        while(simHand.length<maxHandSize && simDeck.length > 0){simHand.push(simDeck.pop())} //fill hand
+        
+        let playsThisTurn=0;let costs = [];
+        for(let card=0;card<simHand.length;card++){
+            costs.push(GetCardCost(simPiles,simHand,simDeck,card));
+        }
+        costs.sort(function(a,b){return a.cost-b.cost});
+        
+        while((playsThisTurn<MinPlaysWithDeck || (costs[0].cost * (1-ratio)) < [1,1,2,4,6,8,10,12][playsThisTurn] * ratio) && costs[0].cost < 500){
+            simPiles[costs[0].pile] = simHand[costs[0].card]
+            simHand.splice(costs[0].card,1);
+
+            costs=[];
+            for(let card=0;card<simHand.length;card++){
+                costs.push(GetCardCost(simPiles,simHand,simDeck,card));
+            }
+            costs.sort(function(a,b){return a.cost-b.cost});
+            simCardsPlayed++;simScore += [1,1,2,4,6,8,10,12][playsThisTurn];playsThisTurn++; 
+            if(simHand.length == 0){break;}
+            if(costs[0] > 500){DeadGame = true;break;}
+        }
+        if(costs.length>0){if(costs[0].cost > 500){DeadGame = true;break}}
+    }//Play cards on the deck while we have cards to play
+    
+    if(curDeckID == deckID){
+        simulationsRun.push({"ratio":ratio,"score":simScore,"cards":simCardsPlayed});
+        simulationsRun.sort(function(a,b){
+            return b.cardsPlayed - a.cardsPlayed
+        })
+        let summaryText = `Highest cards played: ${simulationsRun[0]["cards"]}, score of ${simulationsRun[0]["score"]}, ratio of ${simulationsRun[0]["ratio"]}<br>`;
+        simulationsRun.sort(function(a,b){return b.score-a.score});
+        summaryText += `Highest scoring game: ${simulationsRun[0]["score"]}, played ${simulationsRun[0]["cards"]} cards, ratio of ${simulationsRun[0]["ratio"]}
+        <br>Games played: ${simulationsRun.length}
+        <br>Ratios used: 0`;
+        simulationsRun.sort(function(a,b){return a.ratio-b.ratio});
+        for(let sim=1;sim<simulationsRun.length -1;sim++){
+            summaryText += `, ${parseFloat(simulationsRun[sim].ratio).toFixed(3)}`;
+        }
+        $("SimulationResults").innerHTML = summaryText + ", 1";
+    }//else a change to the deck was made while we were simulating
+}
+
+function GetCardCost(piles,hand,deck,card){
+    let leastBad = {"cost":1000,"pile":0,"card":card};
+    for(let pile=0;pile<4;pile++){
+        if((hand[card]>piles[pile]&&hand[card]!=piles[pile]+10&&pile>1) || (hand[card]<piles[pile]&&hand[card]!=piles[pile]-10&&pile<2)){continue;}
+        //Any card that gets here can be played on this pile
+        let costSavings = 0; 
+        for(let check=Math.min(hand[card],piles[pile])+1;check<Math.max(hand[card],piles[pile]);check++){
+            if(hand.indexOf(check)==-1&&deck.indexOf(check)==-1){
+                costSavings++
+            }
+        }
+        if(hand.indexOf(hand[card] + 10)>-1 || hand.indexOf(hand[card]-10)>-1){
+            //If this card is part of a ten-off pair, cost will go up to play in the wrong direction
+            //and cost will go down to play in the better direction
+            if((hand.indexOf(hand[card] + 10)>-1 && pile > 1) || (hand.indexOf(hand[card] - 10)>-1 && pile < 2)){//it's the proper first play
+                Math.abs(hand[card]-piles[pile])-10-costSavings < leastBad.cost? leastBad = {"cost":Math.abs(hand[card]-piles[pile])-10-costSavings,"pile":pile,"card":card} : leastBad=leastBad;
+            }else{//it's the higher card
+                leastBad = {"cost":Math.abs(hand[card]-piles[pile])+10-costSavings,"pile":pile,"card":card}
+            }
+        }else{
+            if((pile<2 && hand[card] + 10 == piles[pile]) || (pile>1 && hand[card] -10 == piles[pile])){//wrong way cards
+                leastBad = {"cost":-10-costSavings,"pile":pile,"card":card};
+            }else{//normal card costs
+                if(Math.abs(hand[card]-piles[pile])-costSavings < leastBad.cost){
+                    leastBad = {"cost":Math.abs(hand[card]-piles[pile])-costSavings,"pile":pile,"card":card};
+                }
+            }
+            
+        }
+    }
+    return leastBad;
 }
